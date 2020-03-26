5350a0f9de9a91804f7f66081daa0e9063be5b33
LXX3123 <lixingxingchn@126.com>
2020-03-26 20:01:57 +0800

    3.26

---
+++ xcodec/practice/main.c
@@ -0,0 +1,20 @@
+/*
+ 编写一段程序，像下面这样显示读取到的两个整数的乘积。
+
+ 请输入两个整数。
+ 整数1:27↙
+ 整数2:35↙
+ 它们的乘积是945。
+ */
+#include <stdio.h>
+int main(void)
+{
+    int x, y;
+    printf("请输入两个整数\n");
+    printf("整数1: ");
+    scanf("%d", &x);
+    printf("整数2: ");
+    scanf("%d", &y);
+    printf("它们的乘积是%d\n", x * y );
+    return 0;
+}
